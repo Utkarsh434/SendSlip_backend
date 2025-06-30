89b6f1228215d46f0027111a4ea5a6eb0ab650d1
Utkarsh Prakash <114054236+Utkarsh434@users.noreply.github.com>
2025-06-30 15:33:09 +0530

    resolving cors error

--- src/main/java/com/ansh/sendslip/controller/UserController.java
+++ src/main/java/com/ansh/sendslip/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
