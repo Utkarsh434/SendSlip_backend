08f0946c972a45e9229dfe7b0f9a0afc498891a8
Utkarsh Prakash <114054236+Utkarsh434@users.noreply.github.com>
2025-07-13 21:59:30 +0530

    frontend url updation

--- src/main/java/com/ansh/sendslip/config/SecurityConfig.java
+++ src/main/java/com/ansh/sendslip/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://splendid-dolphin-ff231a.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
